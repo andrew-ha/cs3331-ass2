2a0034d4de8d0d40a0a6f210552af92b86005ebc
Raiyan <raiyan.a.zubair@gmail.com>
2017-10-17 12:14:09 +1100

    scanner stuff

--- src/Graph.java
+++ src/Graph.java
@@ -2,10 +2,27 @@
  * Created by andrewha on 10/17/17.
  */
 import java.io.*;
+import java.util.Scanner;
 
 public class Graph {
 
+
+
     public static void main (String[] args) {
+        //Read in the topology
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new File(args[0]));
+
+            while (sc.hasNext()) {
+                System.out.println(sc.nextLine());
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println("No topology file inputted");
+        } finally {
+            if (sc != null) sc.close();
+        }
 
     }
 }
---
+++ src/Node.java
@@ -0,0 +1,7 @@
+package src;
+
+public class Node {
+
+    private String id;
+
+}
